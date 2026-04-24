373df14f77f3e055fb46453dba50bd5c4fce2467
vinaynenavath758 <business@houznext.com>
2026-04-24 11:37:55 +0530

    Errors fixed

--- HZ-backend/src/common/s3/s3.module.ts
+++ HZ-backend/src/common/s3/s3.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 import { S3Service } from './s3.service';
 import { S3Controller } from './s3.controller';
+import { User } from '../../user/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    MulterModule.register({ storage: memoryStorage() }),
+  ],
   controllers: [S3Controller],
   providers: [S3Service],
   exports: [S3Service],
